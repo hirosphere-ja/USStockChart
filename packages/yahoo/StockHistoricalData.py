9ee7ce88c29f3bd391a82d1aaf5b48903bbe83d6
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-09-17 21:28:52 +0900

    日付データに変換する関数を追加及び変更
    日付データに変換するpandasのto_datetime関数を採用
    日付データに変換するstrftime関数から変更

--- packages/yahoo/StockHistoricalData.py
+++ packages/yahoo/StockHistoricalData.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pandas_datareader import data
 from dateutil.relativedelta import relativedelta
 from datetime import date
@@ -10,8 +11,8 @@ from datetime import date
 
 
 def stockDataFromToday(ticker, period):
-	end = date.today().strftime('%d/%m/%Y')
-	start = (date.today() - relativedelta(years=period)).strftime('%d/%m/%Y')
+	end = pd.to_datetime(date.today(), format='%d/%m/%Y', infer_datetime_format=True)
+	start = pd.to_datetime((date.today() - relativedelta(years=period)), format='%d/%m/%Y', infer_datetime_format=True)
 	# print(f"{ticker}・期間：{start}〜{end}")
 	df = data.DataReader(ticker, "yahoo", start, end)
 	return df
@@ -26,9 +27,11 @@ def stockDataFromToday(ticker, period):
 
 
 def stockDataFromDD(ticker, period, d_start):
-	end = d_start
-	start = (d_start - relativedelta(years=period))
+	d_start = pd.to_datetime(d_start, format='%d/%m/%Y', infer_datetime_format=True)
+	end = pd.to_datetime(d_start, format='%d/%m/%Y', infer_datetime_format=True)
+	start = pd.to_datetime((d_start - relativedelta(years=period)), format='%d/%m/%Y', infer_datetime_format=True)
 	# print(f"{ticker}・期間：{start}〜{end}")
 	df = data.DataReader(ticker, "yahoo", start, end)
 	return df
+
 
