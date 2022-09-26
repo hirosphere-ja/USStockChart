f70234659cd48075b79b36bbc1d8e9b19d7f4794
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-09-26 23:43:46 +0900

    不要なコードを削除と修正

--- pages/6_Usstock_WTI_Compare.py
+++ pages/6_Usstock_WTI_Compare.py
@@ -3,7 +3,7 @@ import plotly.graph_objs as go
 import re
 from datetime import date
 from datas.holidays import data
-from packages.fred.StockBondData import stockBondsDataFromDD
+from packages.yahoo.WTIData import WTIDataFromDD
 
 # from packages.investpy.StockHistoricalData import stockDataFromDD
 from packages.yahoo.StockHistoricalData import stockDataFromDD
@@ -12,7 +12,7 @@ st.set_page_config(page_title="株価・金利比較チャート", layout="wide"
 
 rep = r"^[a-zA-Z]{1,5}$"
 
-st.sidebar.title("株価・金利比較チャート")
+st.sidebar.title("株価・WTI原油比較チャート")
 
 if "ticker" not in st.session_state:
 	st.session_state.ticker = ""
@@ -23,19 +23,19 @@ end_day = st.sidebar.date_input("チャートの最終日", date.today())
 vrect_x0 = st.sidebar.date_input("強調表示開始日", date.today())
 vrect_x1 = st.sidebar.date_input("強調表示終了日", date.today())
 bond = st.sidebar.text_input("米国債利回り年数", 10)
-df_bond = stockBondsDataFromDD(bond, period, end_day)
+df_wti = WTIDataFromDD(period, end_day)
 
 if re.match(rep, ticker):
     df = stockDataFromDD(ticker, period, end_day)
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df.index, y=df["Close"], name=f"{ticker.upper()}", yaxis="y1"))
-    fig.add_trace(go.Scatter(x=df_bond.index, y=df_bond[f"DGS{bond}"], name=f"米国債券{bond}年利回り", yaxis="y2",
+    fig.add_trace(go.Scatter(x=df_wti.index, y=df_wti.Close, name="WTI原油", yaxis="y2",
 									connectgaps=True,)) # 途切れたグラフを補完して繋げる
     fig.add_vrect(x0=vrect_x0, x1=vrect_x1, fillcolor="#333333", opacity=0.1, layer="below", line_width=0)
     fig.update_layout(width=1200, height=800, xaxis_rangeslider_visible=False, font_size=20,
                       yaxis1=dict(side="left", title=f"{ticker.upper()} ($)"),
-                      yaxis2=dict(side="right", title=f"米国債券{bond}年利回り", showgrid=False, overlaying="y"))
+                      yaxis2=dict(side="right", title="WTI原油 ($)", showgrid=False, overlaying="y"))
     fig.update_xaxes(rangebreaks=[
 											dict(values=data),
 											dict(bounds=["sat", "mon"])])
