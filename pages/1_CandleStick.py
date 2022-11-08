1c72f6e1977816437a87cc2874905b6912341300
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-11-08 22:47:43 +0900

    ローソク足チャートを機能追加
    トレンドラインを描画できるように機能追加

--- pages/1_CandleStick.py
+++ pages/1_CandleStick.py
@@ -15,8 +15,16 @@ rep = r"^[a-zA-Z]{1,5}$"
 if "ticker" not in st.session_state:
 	st.session_state.ticker = ""
 
+if "lines" not in st.session_state:
+	st.session_state.lines = []
+
 ticker = st.session_state.ticker
 period = st.sidebar.slider("チャートの期間", min_value=1, max_value=5, value=3)
+line = st.sidebar.text_input("トレンドライン")
+st.session_state.lines.append(line)
+print(st.session_state.lines)
+if st.sidebar.button("トレンドラインリセット"):
+	st.session_state.lines = []
 end_day = st.sidebar.date_input("チャートの最終日", date.today())
 vrect_x0 = st.sidebar.date_input("強調表示開始日", date.today())
 vrect_x1 = st.sidebar.date_input("強調表示終了日", date.today())
@@ -28,51 +36,53 @@ else:
     fill_on = None
 
 if re.match(rep, ticker):
-    df = stockDataFromDD(ticker, period, end_day)
-    df = SMA(df, 50)
-    df = SMA(df, 200)
-    df = BB(df, 2)
+	df = stockDataFromDD(ticker, period, end_day)
+	df = SMA(df, 50)
+	df = SMA(df, 200)
+	df = BB(df, 2)
 
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df.SMA50, name="50day SMA"))
-    fig.add_trace(go.Scatter(x=df.index, y=df.SMA200, name="200day SMA"))
-    fig.add_trace(go.Scatter(x=df.index, y=df.Lower, name="-2σ",
-                  fill=None, line=dict(color="#2962ff")))
-    fig.add_trace(go.Scatter(x=df.index, y=df.Upper, name="+2σ",
-                  fill=fill_on, line=dict(color="#2962ff")))
-    fig.add_trace(go.Scatter(x=df.index, y=df.STD, name="base", line=dict(color="#ff6d00")))
-    fig.add_trace(
-        go.Candlestick(
-            name="price",
-            x=df.index,
-            open=df.Open,
-            high=df.High,
-            low=df.Low,
-            close=df.Close,
-            increasing_line_color="darkgreen",
-            decreasing_line_color="red"
-        )
-    )
-    fig.add_vrect(
-        x0=vrect_x0, x1=vrect_x1,
-        fillcolor="#333333", opacity=0.1,
-        layer="below", line_width=0,
-    )
-    fig.update_layout(
-        xaxis_rangeslider_visible=False,
-        width=1200,
-        height=800,
-        title=f"{ticker.upper()} - 米国株価チャート",
-        yaxis_title="USD ($)",
-        font_size=20,
-    )
-    fig.update_xaxes(
-        rangebreaks=[
-            dict(bounds=["sat", "mon"]),  # hide weekends
-            dict(values=data)
-        ]
-    )
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=df.index, y=df.SMA50, name="50day SMA"))
+	fig.add_trace(go.Scatter(x=df.index, y=df.SMA200, name="200day SMA"))
+	fig.add_trace(go.Scatter(x=df.index, y=df.Lower, name="-2σ",
+								fill=None, line=dict(color="#2962ff")))
+	fig.add_trace(go.Scatter(x=df.index, y=df.Upper, name="+2σ",
+								fill=fill_on, line=dict(color="#2962ff")))
+	fig.add_trace(go.Scatter(x=df.index, y=df.STD, name="base", line=dict(color="#ff6d00")))
+	fig.add_trace(
+			go.Candlestick(
+					name="price",
+					x=df.index,
+					open=df.Open,
+					high=df.High,
+					low=df.Low,
+					close=df.Close,
+					increasing_line_color="darkgreen",
+					decreasing_line_color="red"
+			)
+	)
+	for line in st.session_state.lines:
+		fig.add_hline(y=line, line_width=0.8)
+	fig.add_vrect(
+			x0=vrect_x0, x1=vrect_x1,
+			fillcolor="#333333", opacity=0.1,
+			layer="below", line_width=0,
+	)
+	fig.update_layout(
+			xaxis_rangeslider_visible=False,
+			width=1200,
+			height=800,
+			title=f"{ticker.upper()} - 米国株価チャート",
+			yaxis_title="USD ($)",
+			font_size=20,
+	)
+	fig.update_xaxes(
+			rangebreaks=[
+					dict(bounds=["sat", "mon"]),  # hide weekends
+					dict(values=data)
+			]
+	)
 
-    st.plotly_chart(fig, use_container_width=True)
+	st.plotly_chart(fig, use_container_width=True)
 elif(ticker == ""):
-    st.info("トップページでティッカーを入力してください")
+  st.info("トップページでティッカーを入力してください")
